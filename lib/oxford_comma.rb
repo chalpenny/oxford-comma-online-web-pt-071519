82f6295d43481489c40b89d3b9fc82a11c9f9882
Colleen Halpenny <cp.halpenny@gmail.com>
2019-07-24 03:28:06 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
-def oxford_comma(array)
 
+# .pop last item off into its own array
+#add "and" to last element in new array
+# add back to original array
+# convert to string separated by ","
+
+def oxford_comma(array)
+  if
+    array.count == 2
+      array.insert(1, " and ")
+      array.join
+  elsif
+    array.count > 2
+    last_element = array.pop
+    array.join(", ") << ", and " + last_element
+  else
+    array.join
+  end
 end
