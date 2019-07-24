3661b06579f01b10bc6cb6cecd1454b3ffed5269
Colleen Halpenny <cp.halpenny@gmail.com>
2019-07-24 03:00:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
-def oxford_comma(array)
 
+# .pop last item off into its own array
+#add "and" to last element in new array
+# add back to original array
+# convert to string separated by ","
+
+def oxford_comma(array)
+  if
+    array.count == 2
+      array.insert(1, " and ")
+      array.join
+  elsif
+    array.count > 2
+    last_element = array.pop
+    array.join(", ")
+    last_element.unshift(" and ")
+    #last_element.join(", ")
+    #array << last_element
+    #array.join(",")
+  else
+    array.join
+  end
 end
